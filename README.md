627df8dede10deab949a584a081de5fe62b33768
OVNI_modding <OVNI_modding>
2018-03-06 23:49:24 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+kcd_skip_intro by OVNI
+======================
+Mod for Kingdom Come: Deliverance
+- Replaces loud intro video by artwork loading screen.
+- Removes the 2sec delay after loading is completed.
+
+Usage
+=====
+Put zzz_kcd_skip_intro.pak within game's Data folder.
---
+++ build.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3.6
+
+version='1.0.0'
+name='kcd_skip_intro'
+packagePrefix='zzz_'
+outputDir='builds'
+
+#######################################
+
+zipFilename=f'{name}_{version}.zip'
+gamePackageName=f'{packagePrefix}{name}.pak'
+
+import os
+import io
+import zipfile
+
+#######
+# PAK #
+#######
+
+gamePackage = io.BytesIO()
+zout = zipfile.ZipFile( gamePackage, 'w', compression=zipfile.ZIP_STORED )
+
+zout.write( os.path.join('Libs', 'UI', 'UIActions', 'SYS_LoadingVideoScreen.xml') )
+
+zout.close()
+
+#######
+# ZIP #
+#######
+
+zout = zipfile.ZipFile( os.path.join(outputDir, zipFilename), 'w', compression=zipfile.ZIP_DEFLATED )
+
+zout.write('README.md')
+zout.write('LICENSE')
+zout.writestr( os.path.join('Data', gamePackageName), gamePackage.getvalue() )
+
+zout.close()
